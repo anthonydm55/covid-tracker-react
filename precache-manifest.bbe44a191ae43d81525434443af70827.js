18a9d955095f14e96ff055a08c6a94277488d391
anthonydm55 <48017197+anthonydm55@users.noreply.github.com>
2020-07-13 16:52:04 -0500

    Updates

---
+++ precache-manifest.bbe44a191ae43d81525434443af70827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfcf88d49a78fee35b6072f657be9b5",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "64f818cfa02062efee34",
+    "url": "/covid-tracker/static/css/main.700089ad.chunk.css"
+  },
+  {
+    "revision": "d317f7c0f0dc3a2f544b",
+    "url": "/covid-tracker/static/js/2.5ca6919f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.5ca6919f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f818cfa02062efee34",
+    "url": "/covid-tracker/static/js/main.a236a14d.chunk.js"
+  },
+  {
+    "revision": "4ab6cc6a6673a41c1175",
+    "url": "/covid-tracker/static/js/runtime-main.51b14c82.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
